5e5bb0be1d6da6d6e9a99b3fbba1039f9a3a0444
Larin13 <larin.plt@gmail.com>
2024-11-01 12:11:03 +0200

    roter, redux

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import '../assets/styles/main.scss';
+import { ErrorBoundary } from '../shared';
+import AppRoutes from '../routes/AppRoutes';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-
-export default App
+    <ErrorBoundary>
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
+    </ErrorBoundary>
+  );
+};
+export default App;
